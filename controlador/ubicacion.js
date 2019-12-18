0f664c9490855d4ea75f3fe3f649ac3f5da7f09c
esteban <esteban.icc@gmail.com>
2019-12-18 13:55:26 -0500

    make it better 3

--- controlador/ubicacion.js
+++ controlador/ubicacion.js
@@ -5,14 +5,14 @@ const getCiudadLatLon = async(nombre) => {
     const ciudad = encodeURI(nombre);
 
     const instance = axios.create({
-        baseURL:`https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ciudad}`,
-        headers: {'X-RapidAPI-Key':'7fa8905a9dmsh3149b9f9b1f87c4p181b55jsn4742d235c097'}
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ciudad}`,
+        headers: { 'X-RapidAPI-Key': '8ac05538ebmsh1d8d2bb9ae49d50p1737a9jsn3064fa337c89' }
     });
 
     const resp = await instance.get();
 
     if (resp.data.Results.length === 0) {
-        throw new Error(`No existe resultados para ${nombre}`,e);
+        throw new Error(`No existe resultados para ${nombre}`);
     }
 
     const data = resp.data.Results[0];
